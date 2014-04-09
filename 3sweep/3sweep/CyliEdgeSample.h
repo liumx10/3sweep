df87252d4e39dc89ac23ae58a96798fe5362416e
刘梦醒 <1047804485@qq.com>
2014-04-09 21:59:38 +0800

    commit

--- 3sweep/3sweep/CyliEdgeSample.h
+++ 3sweep/3sweep/CyliEdgeSample.h
@@ -14,32 +14,21 @@ public:
 	void sample();
 	std::vector<Vector3D> get3DSample();
 
-	Vector2D getCenter() { return center; }
-	Vector2D getNormal() { return normal; }
 	double getA() { return a; }
 	double getB() { return b; }
-	Vector2D* getEndpoint() { return endpoint; }
 
-	void setCenter(Vector2D v) { center = v; }
-	void setNormal(Vector2D v) { normal = v; }
 	void setA(double A) { a = A; }
 	void setB(double B) { b = B; }
-	void setEndpoint(Vector2D* v) { endpoint[0] = v[0]; endpoint[1] = v[1]; }
 
 	void clear(){ samples.clear(); }
 
 	std::vector<Vector2D> samples;  
 private:
-	Vector2D center;    
-	Vector2D normal;   
-	Vector2D[2] endpoint;
 
 	double a;  // two arguments for a ellipse 
 	double b;
 
 	Vector2D getEllipsePoint(int i);
 };
-
-int CyliEdgeSample::sampleNum;
 
 #endif
